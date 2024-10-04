556a0415ce6884a071b6dd27d44c121db8cd2113
Jibran <jibran.linkedmatrix@gmail.com>
2024-10-04 10:05:16 -0400

    Dunder Method

--- dunder_method.py
+++ dunder_method.py
@@ -4,5 +4,11 @@ class square:
 
     def __repr__(self) :
         return str(self.area)
+    def __add__(self,other):
+        return str(self.area + other.area)
+
 s1 = square(90)
+s2 = square(12)
 print(s1)
+print(s1+s2)
+
