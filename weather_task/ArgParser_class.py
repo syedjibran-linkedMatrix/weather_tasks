f029df8bbb650070a7595e39f25bdcf2156b0e53
syed jibran <jibran.linkedmatrix@gmail.com>
2024-10-10 15:29:31 +0500

    Task

---
+++ weather_task/ArgParser_class.py
@@ -0,0 +1,29 @@
+import argparse 
+class ArgParser:
+    def __init__(self):
+        self.parser = argparse.ArgumentParser(description="Weather Data Analysis")
+
+        # Adding arguments for date range, input file, and output file
+        self.parser.add_argument(
+            '--start-date', type= str, required=True, help="Start date for the analysis (YYYY-MM-DD)"
+        )
+        self.parser.add_argument(
+            '--end-date', type=str, required=True, help="End date for the analysis (YYYY-MM-DD)"
+        )
+        self.parser.add_argument(
+            '--input-file', type=str, required=True, help="Input CSV file with weather data"
+        )
+        self.parser.add_argument(
+            '--output-file', type=str, required=True, help="Output CSV file for results"
+        )
+
+    def parse_args(self):
+        return self.parser.parse_args()
+    
+if __name__ == "__main__":
+    arg_parser = ArgParser()
+    args = arg_parser.parse_args()
+
+    
+#/bin/python3 /home/lm/Downloads/weather_tasks/weather_task/highest_temp_day.py --start-date 2016-01-03 --end-date 2016-12-25 --input-file /home/lm/Downloads/weather_1.csv --output-file /home/lm/Downloads/output.csv
+
